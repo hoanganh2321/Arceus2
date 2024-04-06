89060c7f05ec00794b78a44adca433998a92c35d
hoanganh2321 <131464214+hoanganh2321@users.noreply.github.com>
2024-04-06 21:29:30 +0700

    Update mainn.py

--- mainn.py
+++ mainn.py
@@ -9,12 +9,13 @@ bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 async def on_ready():
     print(f"Logged in as {bot.user}")
     print("==========================")
-    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="StickX"))
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name="Code By Kaioshin"))
 
-@bot.command(name="key", description="Bypass Fluxus Key For Your")
+@bot.command(name="arceus", description="Bypass Arceus Key For Your")
 async def fluxus(ctx, link: str):
     hwid = link.split("HWID=")[-1]
-    url = f"https://stickx.top/api-fluxus/?hwid={hwid}"
+    
+    url = f"https://stickx.top/api-arceusx/?hwid={hwid}&api_key=E99l9NOctud3vmu6bPne"
 
     response = requests.get(url)
 

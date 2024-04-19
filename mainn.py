a0f5dcaf148786c21a3df0dbafc938a9480b17de
hoanganh2321 <131464214+hoanganh2321@users.noreply.github.com>
2024-04-19 19:47:08 +0700

    Update mainn.py

--- mainn.py
+++ mainn.py
@@ -2,26 +2,27 @@ import discord
 from discord.ext import commands
 import requests
 
-BotToken = 'MTE1ODMxMTY0NDYzNjY0NzQzNA.GTNJS3.mS7d55vtwdJXD9RYdgNKwGfPxyfgPJjXHKpsHY'
+BotToken = 'MTIzMDgyMjMwNTg3NDA1MTEwMw.GR_8Zo.XrWs-1ZL2ga7HMY8LPUfdqKeGec-jjD73ictX4'
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}")
     print("==========================")
-    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name="Code By Kaioshin"))
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name="Code by Kaioshin"))
 
-@bot.command(name="arceus", description="Bypass Arceus Key For Your")
+@bot.command(name="key", description="Bypass arceusx Key For Your")
 async def fluxus(ctx, link: str):
-    hwid = link.split("HWID=")[-1]
+    hwid = link.split("hwid=")[-1].split("&")[0]  
     
     url = f"https://stickx.top/api-arceusx/?hwid={hwid}&api_key=E99l9NOctud3vmu6bPne"
-
+    
     response = requests.get(url)
-
+    
     if response.status_code == 200:
         data = response.json()
-        await ctx.reply(f"{data['key']}")
+        await ctx.reply(f" > # _ {data['key']}_")
+        
     else:
         print(f"Failed to retrieve data from API with status code {response.status_code}")
         await ctx.reply(f"Failed to retrieve data from API with status code {response.status_code}") 
